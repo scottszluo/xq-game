1ed2dd3f241c396e842708158699bbdfe90a2dbf
LuPindong <lupindong@gmail.com>
2017-07-09 23:23:43 +0800

    更新libgdx版本

--- core/src/main/java/net/lovexq/game/core/GameMain.java
+++ core/src/main/java/net/lovexq/game/core/GameMain.java
@@ -1,45 +1,52 @@
 package net.lovexq.game.core;
 
-import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GameMain implements ApplicationListener {
-	Texture texture;
-	SpriteBatch batch;
-	float elapsed;
+    Texture texture;
+    TextureRegion textureRegion;
+    BitmapFont font;
+    SpriteBatch batch;
+    float elapsed;
 
-	@Override
-	public void create () {
-		texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
-		batch = new SpriteBatch();
-	}
+    @Override
+    public void create() {
+        texture = new Texture(Gdx.files.internal("images/Base.png"));
+        textureRegion = new TextureRegion(texture, 0, 0, 800, 600);
+        batch = new SpriteBatch();
+    }
 
-	@Override
-	public void resize (int width, int height) {
-	}
+    @Override
+    public void resize(int width, int height) {
+    }
 
-	@Override
-	public void render () {
-		elapsed += Gdx.graphics.getDeltaTime();
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(texture, 100+100*(float)Math.cos(elapsed), 100+25*(float)Math.sin(elapsed));
-		batch.end();
-	}
+    @Override
+    public void render() {
+        elapsed += Gdx.graphics.getDeltaTime();
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(textureRegion, 0, 0, 800, 600);
+        batch.end();
+    }
 
-	@Override
-	public void pause () {
-	}
+    @Override
+    public void pause() {
+    }
 
-	@Override
-	public void resume () {
-	}
+    @Override
+    public void resume() {
+    }
 
-	@Override
-	public void dispose () {
-	}
+    @Override
+    public void dispose() {
+        texture.dispose();
+        batch.dispose();
+    }
 }
--- desktop/src/main/java/net/lovexq/game/java/GameMainDesktop.java
+++ desktop/src/main/java/net/lovexq/game/java/GameMainDesktop.java
@@ -2,12 +2,14 @@ package net.lovexq.game.java;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 import net.lovexq.game.core.GameMain;
 
 public class GameMainDesktop {
-	public static void main (String[] args) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GameMain(), config);
-	}
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "新决战朝鲜";
+        config.width = 800;
+        config.height = 600;
+        new LwjglApplication(new GameMain(), config);
+    }
 }
